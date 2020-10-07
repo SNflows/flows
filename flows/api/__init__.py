6fc5cea5d67d8b7b6ddc67a94dfac9531504a8ad
Rasmus Handberg <rasmush@phys.au.dk>
2020-10-07 12:32:36 +0200

    Improvements to visibility plotting

--- flows/api/__init__.py
+++ flows/api/__init__.py
@@ -5,7 +5,7 @@
 from .targets import get_targets, get_target
 from .datafiles import get_datafile, get_datafiles
 from .catalogs import get_catalog, get_catalog_missing
-from .sites import get_site
+from .sites import get_site, get_all_sites
 from .upload_photometry import upload_photometry
 from .set_photometry_status import set_photometry_status
 from .filters import get_filters
