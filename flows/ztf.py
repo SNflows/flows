3865c60969ee8411f3057f068926f312660cad37
Rasmus Handberg <rasmush@phys.au.dk>
2021-04-19 12:09:54 +0200

    Fix ZTF queries after their API changed

--- flows/ztf.py
+++ flows/ztf.py
@@ -10,20 +10,23 @@ https://alerceapi.readthedocs.io/
 
 import numpy as np
 import astropy.units as u
-from astropy.coordinates import Angle
+from astropy.coordinates import Angle, SkyCoord
 from astropy.table import Table
 import datetime
 import requests
 from . import api
 
 #--------------------------------------------------------------------------------------------------
-def query_ztf_id(coo_centre, radius=24*u.arcmin):
+def query_ztf_id(coo_centre, radius=1*u.arcmin):
 	"""
 	Query ALeRCE ZTF api to lookup ZTF identifier.
 
+	In case multiple identifiers are found within the search cone, the one
+	closest to the centre is returned.
+
 	Parameters:
 		coo_centre (:class:`astropy.coordinates.SkyCoord`): Coordinates of centre of search cone.
-		radius (Angle, optional): Search radius. Default 24 arcmin.
+		radius (Angle, optional): Search radius. Default 1 arcmin.
 
 	Returns:
 		str: ZTF identifier.
@@ -37,28 +40,36 @@ def query_ztf_id(coo_centre, radius=24*u.arcmin):
 
 	# Make json query for Alerce query API
 	query = {
-		"records_per_pages": 20,
-		"query_parameters": {
-			"coordinates": {
-				"ra": coo_centre.ra.deg,
-				"dec": coo_centre.dec.deg,
-				"sr": Angle(radius).deg
-			}
-		}
+		'ra': coo_centre.ra.deg,
+		'dec': coo_centre.dec.deg,
+		'radius': Angle(radius).arcsec,
+		'page_size': 20,
+		'count': True
 	}
 
 	# Run http POST json query to alerce following their API
-	res = requests.post('https://ztf.alerce.online/query', json=query)
+	res = requests.get('https://api.alerce.online/ztf/v1/objects', params=query)
 	res.raise_for_status()
-
-	# If successful, get objectid of first object
 	jsn = res.json()
+
+	# If nothing was found, return None:
 	if jsn['total'] == 0:
 		return None
 
-	names = list(jsn['result'].keys())
-	oid = names[0]
-	return oid
+	# Start by removing anything marked as likely stellar-like source:
+	results = jsn['items']
+	results = [itm for itm in results if not itm['stellar']]
+
+	# Find target closest to the centre:
+	coords = SkyCoord(
+		ra=[itm['meanra'] for itm in results],
+		dec=[itm['meandec'] for itm in results],
+		unit='deg',
+		frame='icrs')
+
+	indx = np.argmin(coords.separation(coo_centre))
+
+	return results[indx]['oid']
 
 #--------------------------------------------------------------------------------------------------
 def download_ztf_photometry(targetid):
@@ -83,13 +94,13 @@ def download_ztf_photometry(targetid):
 		return None
 
 	# Query ALERCE for detections of object based on oid
-	res = requests.post('https://ztf.alerce.online/get_detections', json={'oid': oid})
+	res = requests.get(f'https://api.alerce.online/ztf/v1/objects/{oid:s}/detections')
 	res.raise_for_status()
 	jsn = res.json()
 
 	# Create Astropy table, cut out the needed columns
 	# and rename columns to something better for what we are doing:
-	tab = Table(data=jsn['result']['detections'])
+	tab = Table(data=jsn)
 	tab = tab[['fid', 'mjd', 'magpsf_corr', 'sigmapsf_corr']]
 	tab.rename_column('fid', 'photfilter')
 	tab.rename_column('mjd', 'time')
@@ -114,6 +125,6 @@ def download_ztf_photometry(targetid):
 	tab.meta['target_name'] = target_name
 	tab.meta['targetid'] = targetid
 	tab.meta['ztf_id'] = oid
-	tab.meta['last_updated'] = datetime.datetime.utcnow().isoformat()
+	tab.meta['last_updated'] = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
 
 	return tab
