3e5a8034e890967cf0204b9ca9505f650e7fa8f1
Rasmus Handberg <rasmush@phys.au.dk>
2021-06-10 10:43:36 +0200

    Less format changing

--- flows/catalogs.py
+++ flows/catalogs.py
@@ -454,7 +454,7 @@ def download_catalog(target=None, radius=24*u.arcmin, radius_ztf=3*u.arcsec, dis
 			target_name = row['target_name']
 			dd = row['discovery_date']
 			if dd is not None:
-				dd = Time(dd.isoformat(), format='isot', scale='utc')
+				dd = Time(dd, format='datetime', scale='utc')
 
 			# Coordinate of the target, which is the centre of the search cone:
 			coo_centre = SkyCoord(ra=row['ra'], dec=row['decl'], unit=u.deg, frame='icrs')
