142cf61343b41f7515bb4a095167913307be5da4
Emir Karamehmetoglu <emir.k@phys.au.dk>
2022-06-29 00:51:35 +0200

    remove old tns test setup

--- tests/test_tns.py
+++ tests/test_tns.py
@@ -10,14 +10,13 @@ import pytest
 import os
 import datetime
 from astropy.coordinates import SkyCoord
-from conftest import capture_cli
 from flows import tns
 
 
 # --------------------------------------------------------------------------------------------------
 @pytest.mark.skipif(os.environ.get('CI') == 'true',
                     reason="Disabled on GitHub Actions to avoid too many requests HTTP error")
-def test_tns_search(SETUP_CONFIG):
+def test_tns_search():
     coo_centre = SkyCoord(ra=191.283890127, dec=-0.45909033652, unit='deg', frame='icrs')
     res = tns.tns_search(coo_centre)
 
@@ -29,7 +28,7 @@ def test_tns_search(SETUP_CONFIG):
 # --------------------------------------------------------------------------------------------------
 @pytest.mark.skipif(os.environ.get('CI') == 'true',
                     reason="Disabled on GitHub Actions to avoid too many requests HTTP error")
-def test_tns_get_obj(SETUP_CONFIG):
+def test_tns_get_obj():
     res = tns.tns_get_obj('2019yvr')
 
     print(res)
@@ -40,7 +39,7 @@ def test_tns_get_obj(SETUP_CONFIG):
 # --------------------------------------------------------------------------------------------------
 @pytest.mark.skipif(os.environ.get('CI') == 'true',
                     reason="Disabled on GitHub Actions to avoid too many requests HTTP error")
-def test_tns_get_obj_noexist(SETUP_CONFIG):
+def test_tns_get_obj_noexist():
     res = tns.tns_get_obj('1892doesnotexist')
     print(res)
     assert res is None
@@ -52,7 +51,7 @@ def test_tns_get_obj_noexist(SETUP_CONFIG):
 @pytest.mark.parametrize('date_begin,date_end',
                          [('2019-01-01', '2019-02-01'), (datetime.date(2019, 1, 1), datetime.date(2019, 2, 1)),
                           (datetime.datetime(2019, 1, 1, 12, 0), datetime.datetime(2019, 2, 1, 12, 0))])
-def test_tns_getnames(SETUP_CONFIG, date_begin, date_end):
+def test_tns_getnames(date_begin, date_end):
     names = tns.tns_getnames(date_begin=date_begin, date_end=date_end, zmin=0, zmax=0.105, objtype=3)
 
     print(names)
@@ -64,21 +63,21 @@ def test_tns_getnames(SETUP_CONFIG, date_begin, date_end):
 
 
 # --------------------------------------------------------------------------------------------------
-def test_tns_getnames_wronginput(SETUP_CONFIG):
+def test_tns_getnames_wronginput():
     # Wrong dates should result in ValueError:
     with pytest.raises(ValueError):
         tns.tns_getnames(date_begin=datetime.date(2019, 1, 1), date_end=datetime.date(2017, 1, 1))
 
 
-# --------------------------------------------------------------------------------------------------
-@pytest.mark.skipif(os.environ.get('CI') == 'true',
-                    reason="Disabled on GitHub Actions to avoid too many requests HTTP error")
-def test_run_querytns(SETUP_CONFIG):
-    # Run the command line interface:
-    out, err, exitcode = capture_cli('run_querytns.py')
-    assert exitcode == 0
-
-
-# --------------------------------------------------------------------------------------------------
-if __name__ == '__main__':
-    pytest.main([__file__])
+# # --------------------------------------------------------------------------------------------------
+# @pytest.mark.skipif(os.environ.get('CI') == 'true',
+#                     reason="Disabled on GitHub Actions to avoid too many requests HTTP error")
+# def test_run_querytns():
+#     # Run the command line interface:
+#     out, err, exitcode = capture_cli('run_querytns.py')
+#     assert exitcode == 0
+#
+#
+# # --------------------------------------------------------------------------------------------------
+# if __name__ == '__main__':
+#     pytest.main([__file__])
