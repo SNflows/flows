ffe9143f5ef62641e3d1f4c805c2866ea367619a
Emir Karamehmetoglu <emir.k@phys.au.dk>
2022-06-28 23:06:51 +0200

    fix tendrils setup

--- tests/setup_tendrils.py
+++ tests/setup_tendrils.py
@@ -4,6 +4,12 @@ import os
 
 def set_up_tendrils_in_ci():
     # Set UP API key
+    config = utils.load_config()
+    if config.has_option('api', 'token'):
+        token = config.get('api', 'token')
+        if token not in [None, '', 'None', 'test']:  # if not, we probably have a token.
+            return
+
     os.environ.get('FLOWS_API_TOKEN')
     utils.set_api_token(token=os.environ.get('FLOWS_API_TOKEN'), overwrite=True)
 
