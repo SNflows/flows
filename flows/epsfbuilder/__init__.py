0f8f1c05e0413b4b534a1a6f04feeb5fe1d6171a
Kim4213 <kimphan95@hotmail.com>
2023-06-16 13:30:45 +0200

    test1

--- flows/epsfbuilder/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# flake8: noqa
-
-from .epsfbuilder import FlowsEPSFBuilder, verify_epsf
--- flows/instruments/__init__.py
+++
@@ -1,4 +0,0 @@
-from .instruments import INSTRUMENTS, Instrument, verify_coordinates
-from .sites import Site
-
-__all__ = ["INSTRUMENTS", "Instrument", "verify_coordinates", "Site"]
